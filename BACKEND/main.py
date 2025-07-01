422064c94054dfa0b0692411845a1ea8a8d79e38
Veerman14 <hasibuanfirman1404@gmail.com>
2025-07-02 02:42:10 +0700

    complete project

--- BACKEND/main.py
+++ BACKEND/main.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI
+from routers import router
+from core.config import settings
 
-app = FastAPI()
 
+app = FastAPI(title=settings.APP_NAME)
+
+app.include_router(router.router, prefix="/api/v1", tags=["api"])
 @app.get("/")
 def read_root():
     return {"message": "Welcome to COVID19 Sentiment Prediction API. Go to /docs for documentation."}
